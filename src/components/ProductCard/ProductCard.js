3456b5a634fd9baedf62eac85504421cc184f965
Masa Zablah <masazablah2@gmail.com>
2024-05-22 01:22:29 +0300

    add to cart button

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './ProductCard.css';
+import AddToCartIcon from '../add-to-cart.png'
 
 const ProductCard = ({ product}) => {
   return (
@@ -11,6 +12,9 @@ const ProductCard = ({ product}) => {
         <h3 className="product-name">{product.name}</h3>
         <div className="product-details">
           <span className="product-price">${product.price.toFixed(2)}</span>
+          <button className="add-to-cart-button" >
+            <img src={AddToCartIcon} alt="Add to Cart" className="cart-icon" />
+          </button>
         </div>
       </div>
     </div>
